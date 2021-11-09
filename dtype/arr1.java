d007f9812a76564933968fa524ccb5a4b8441b43
vk3690 <vikas07896@gmail.com>
2021-11-09 13:55:38 +0530

    arrays

---
+++ dtype/arr1.java
@@ -0,0 +1,18 @@
+public class arr1
+{
+    public static void main(String[] args)
+    {
+        int[] ar= new int[20];
+        for(int i=0;i<20;i++)
+        {
+            ar[i]=i;
+        }
+        int i=0;
+        while(i<20)
+        {
+            System.out.println(ar[i]);
+            i++;
+        }
+
+    }
+}
