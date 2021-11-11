52de70b3dda95fc4cec4b5acd5d77887d8d24fbe
vk3690 <vikas07896@gmail.com>
2021-11-11 11:38:39 +0530

    abstrction added1

---
+++ oop/Abdemo.java
@@ -0,0 +1,37 @@
+public class Abdemo 
+{
+        public static void main(String[] args)
+        {
+            phone3 obj1=new phone3();
+            obj1.wifi();
+            obj1.camera();
+            obj1.move();
+        }
+}
+abstract class phone1
+{
+    public void camera()
+    {
+        System.out.println("camera...");
+    }
+    public abstract void move();
+    public abstract void wifi();
+
+}
+ 
+ abstract class phone2 extends phone1
+{
+    public void wifi()
+    {
+        System.out.println("wifi.....");
+    }
+}
+
+class phone3 extends phone2
+{
+    public void move()
+    {
+        System.out.println("moving.....");
+    }
+
+}
